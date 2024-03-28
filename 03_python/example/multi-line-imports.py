b763e6243b6c7752ae42acc2b4e7a81c171ede2c
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-03-28 17:37:40 +0000

    add base calasses

---
+++ 03_python/example/multi-line-imports.py
@@ -0,0 +1,5 @@
+import os
+from os import (
+    path,
+    remove
+)
---
+++ 03_python/main.py
@@ -0,0 +1,12 @@
+from antlr4 import *
+from Python3Lexer import Python3Lexer
+from Python3Parser import Python3Parser
+
+code = open('multi-line-imports.py', 'r').read()
+codeStream = InputStream(code)
+lexer = Python3Lexer(codeStream)
+
+tokens = lexer.getAllTokens()
+
+for t in tokens:
+    print(t.text, t.type)
